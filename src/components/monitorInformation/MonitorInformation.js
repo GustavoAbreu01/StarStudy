76d0dbdbdade0809e9a596f0e0e46c7c2781f307
Gustavo Abreu <137020767+GustavoAbreu01@users.noreply.github.com>
2024-05-13 21:41:19 -0300

    Criação de Componentes de Suporte

---
+++ src/components/monitorInformation/MonitorInformation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './MonitorInformation.css'
+
+function MonitorInformation() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MonitorInformation
