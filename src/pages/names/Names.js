f2515de88e00a19ff157050c76cb3ba1992a019d
Gustavo Abreu <137020767+GustavoAbreu01@users.noreply.github.com>
2024-06-03 19:01:54 -0300

    Att

---
+++ src/pages/names/Names.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Names.css'
+
+function Names() {
+  return (
+    <div className='box_names'>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+    </div>
+  )
+}
+
+export default Names
