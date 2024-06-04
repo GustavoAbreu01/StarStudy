91c2fefec722ce847b036bf76191e2ca11ddde92
Gustavo Abreu <137020767+GustavoAbreu01@users.noreply.github.com>
2024-06-03 21:01:54 -0300

    Atualização Link

--- src/components/bannerMain/BannerMain.js
+++ src/components/bannerMain/BannerMain.js
@@ -5,6 +5,8 @@ import BannerMainL from '../../assets/img/BannerMainLight.png'
 import ThemesBanners from '../themesBanners/ThemesBanners'
 import { Link } from 'react-router-dom'
 
+import { FaUserAlt } from "react-icons/fa";
+
 function BannerMain() {
   return (
     <>
@@ -13,9 +15,12 @@ function BannerMain() {
           <p className='title_main'>Introdução a Programção e Robótica</p>
           <p className='desc_main'>Este material foi criado para fornecer uma base sólida nos conceitos fundamentais dessas áreas. Você aprenderá os princípios da lógica de programação, estruturas de dados e algoritmos, bem como a aplicação prática desses conhecimentos na construção e programação de robôs.</p>
           <ThemesBanners />
-          <Link to="/content/1" class="button_main" role="button">Saiba mais</Link>
+          <div className='buttons_layout'>
+            <Link to="/content/1" class="button_main" role="button">Saiba mais</Link>
+            <Link to="/name" class="button_main_user" role="button"><FaUserAlt/></Link>
+          </div>
         </div>
-        <img className='banner_main_image' id="imgCenterMain" src={BannerMainL}/>
+        <img className='banner_main_image' id="imgCenterMain" src={BannerMainL} />
       </div>
     </>
   )
